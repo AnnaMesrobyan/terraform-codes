6962ea94fcb0d27c5d8bb2ab9e071b1fe738d72d
AnnaMesrobyan <126562129+AnnaMesrobyan@users.noreply.github.com>
2023-09-25 22:09:00 +0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "webword" {
   user_data                   = file("user_data.sh")
 
   tags = {
-    Name = "Web_ansible"
+    Name = "ansible_host1"
   }
 }
 
@@ -26,73 +26,7 @@ resource "aws_instance" "web" {
   user_data                   = file("user_data.sh")
 
   tags = {
-    Name = "Web"
+    Name = "ansible-host2"
   }
 }
 
-#private and DB
-resource "aws_instance" "Ansible_master" {
-  ami                    = "ami-04e601abe3e1a910f"
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.private.id
-  vpc_security_group_ids = ["${aws_security_group.mydbsecurity.id}"]
-  key_name               = "Frank"
-  availability_zone      = "eu-central-1a"
-  user_data              = file("user_data.sh")
-
-
-  tags = {
-    Name = "Ansible_master"
-  }
-}
-
-resource "aws_instance" "Web1" {
-  ami                    = "ami-04e601abe3e1a910f"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private1.id
-  vpc_security_group_ids = ["${aws_security_group.mydbsecurity.id}"]
-  key_name               = "Frank"
-  availability_zone      = "eu-central-1b"
-  user_data              = file("user_data.sh")
-
-
-  tags = {
-    Name = "Web1"
-  }
-}
-
-resource "aws_instance" "Web_DB" {
-  ami                    = "ami-04e601abe3e1a910f"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_DB.id
-  vpc_security_group_ids = ["${aws_security_group.mydbsecurity.id}"]
-  key_name               = "Frank"
-  availability_zone      = "eu-central-1a"
-
-
-
-  tags = {
-    Name = "Web_DB"
-  }
-}
-
-resource "aws_instance" "Web_DB1" {
-  ami                    = "ami-04e601abe3e1a910f"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_DB1.id
-  vpc_security_group_ids = ["${aws_security_group.mydbsecurity.id}"]
-  key_name               = "Frank"
-  availability_zone      = "eu-central-1b"
-
-
-
-  tags = {
-    Name = "Web_DB1"
-  }
-}
-
-# resource "null_resource" "run_playbook" {
-#   provisioner "local-exec" {
-#     command = "sudo ansible-playbook  /root/Ansible_ws/mainplaybook.yaml"
-#   }
-# }
